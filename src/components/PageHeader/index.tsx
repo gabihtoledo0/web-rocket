343d79d322da6017be7bf58994252ced2a567ffa
Gabriela  Toledo Prado <gabriela.prado@youse.com.br>
2020-08-05 21:27:56 -0300

    finished page of available proffs

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,26 +1,28 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
 import logoImg from "../../assets/images/logo.svg";
 import backIcon from "../../assets/images/icons/back.svg";
+import "./styles.css";
 
 interface PageHeaderProps {
   title: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
-  return(
+  return (
     <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Voltar"/>
-          </Link>
-          <img src={logoImg} alt="Proffy"/>
-        </div>
-        <div className="header-contents">
-          <strong>{props.title}</strong>
-        </div>
-      </header>
-  )
-}
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="Voltar" />
+        </Link>
+        <img src={logoImg} alt="Proffy" />
+      </div>
+      <div className="header-contents">
+        <strong>{props.title}</strong>
+        {props.children}
+      </div>
+    </header>
+  );
+};
 
 export default PageHeader;
